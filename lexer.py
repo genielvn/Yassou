3ead14effbfb8febf0432074b7cd5bddbe115251
lezzthanthree <arisatominato48@gmail.com>
2023-11-19 13:50:49 +0800

    Added number lexer rules

--- lexer.py
+++ lexer.py
@@ -22,23 +22,38 @@ class Lexer():
             token_queue = []
 
             for char_num, char in enumerate(line):
+                print(repr(char))
                 if char in WHITESPACE:
                     continue
                 elif char in SYM_DICT:
-                    token_queue.append(Token(SYM_DICT[char], char, line_num, char_num))
+                    if char == '.' and self.checkPreviousTokenType(token_queue) == 'INTEGER':
+                        value = token_queue[-1].getValue()
+                        token_queue.pop()
+                        token_queue.append(DecimalToken(str(value) + char, line_num, char_num))
+                    else:
+                        token_queue.append(Token(SYM_DICT[char], char, line_num, char_num))
                     continue
                 elif char in DELIMITER_DICT:
                     token_queue.append(Token(DELIMITER_DICT[char], char, line_num, char_num))
                     continue
                 elif char in DIGITS:
-                    token_queue.append(self.make_number(line_num, char_num))
+                    if self.checkPreviousTokenType(token_queue) == 'INTEGER' or self.checkPreviousTokenType(token_queue) == 'DECIMAL':
+                        token_queue[-1].concatValue(char)
+                    else:
+                        token_queue.append(IntegerToken(char, line_num, char_num))
                     continue
                 elif char == '\n':
                     continue
                 else:
                     raise InvalidCharacterError(line_num, char_num, char)
-            
+                
             self.addTokenToSymTable(token_queue)
+
+
+    def checkPreviousTokenType(self, token_queue):
+        if not token_queue:
+            return None
+        return token_queue[-1].getType()
 
     def getSymTable(self, file):
         if not self.symtable:

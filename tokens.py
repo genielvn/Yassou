dd3fb7feb42de6a4629c85530871307be279fb38
daestrella <da.estrella@yahoo.com>
2023-11-21 09:53:04 +0800

    fixed 0-based line and char numbers

--- tokens.py
+++ tokens.py
@@ -7,7 +7,7 @@ class Token():
     def __init__(self, token_type, value, line_num, char_num):
         self.type       = token_type
         self.value      = value
-        self.location   = {'line_num' : line_num, 'char_num' : char_num}
+        self.location   = {'line_num' : line_num+1, 'char_num' : char_num+1}
 
     def __repr__(self):
         return f'({self.type}, {self.value}, {self.location})'

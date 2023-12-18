111f69716378be2c5f72f687a84bddd897cd2742
lezzthanthree <arisatominato48@gmail.com>
2023-12-18 15:22:45 +0800

    added file extension check

--- yassou-c/main.c
+++ yassou-c/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <ctype.h>
 #include <string.h>
 #include "error.h"
 #include "constants.h"
-
 
 char filename[];
 
@@ -23,18 +23,24 @@ int main(int argc, char **argv)
     }
 
     Args arguments;
+    bool file_found = false;
 
     for (int i = 1; i < argc; i++)
     {
         if (argv[i][0] != '-')
         {
+            if (file_found) {
+                printf("ERROR : Only one file for compiling.\n");
+                return -1;
+            }
             strcpy(filename, argv[i]);
+            file_found = true;
             continue;
         }
         switch (argv[i][1])
         {
             case 'd':
-                if (!arguments.debugging) printf("DEBUG : Debugging enabled");
+                if (!arguments.debugging) printf("DEBUG : Debugging enabled\n");
                 arguments.debugging = true;
                 break;
             
@@ -44,10 +50,16 @@ int main(int argc, char **argv)
         }
     }
 
-    if (strcmp(filename, "") == 0)
+    if (!file_found)
     {
         printf("usage: yassou [FILE] [-d]\n");
         FILEPROVIDED;
+        return -1;
+    }
+
+    if (!get_file_extension(filename, ".yass"))
+    {
+        printf("ERROR : File %s is not a valid YASS file.", filename);
         return -1;
     }
 
@@ -55,3 +67,18 @@ int main(int argc, char **argv)
     FILEEXISTCHECK;
 }
 
+
+int get_file_extension(const char* filename, const char* ext)
+{
+    int path_idx = strlen(filename) - 1;
+    int ext_idx = strlen(ext) - 1;
+
+    // search extension from the last character
+    for (; path_idx >= 0 && ext_idx >= 0; path_idx--, ext_idx--) {
+        printf("%c=%c", filename[path_idx], ext[ext_idx]);
+        if (tolower(filename[path_idx]) != tolower(ext[ext_idx])) {
+            return false;
+        }
+    }
+    return true;
+}
